b30caf0bc28a3b4cd22c5d2d4beae067e86963ab
Tasavvuf Gori <tasavvufg@gmail.com>
2026-08-14 18:01:39 +0530

    Write calculate(a: unknown, b: unknown, op: "add" | "subtract"): number | string — narrow a and b to ensure both are number before doing the operation. If either isn't a number, return "Invalid operands".

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
-function parseApiResponse(data: unknown): string {
-if (typeof data === "string"){
-    return data
-}
-if (typeof data === "number"){
-    return `${data}`
-}
-if (Array.isArray(data)) {
-    for (const element of data) {
-        if (typeof element !== "string" && typeof element !== "number") {
-            return "Invalid data"
-        }
+function calculate(a: unknown, b: unknown, op: "add" | "subtract"): number | string {
+    if (typeof a === "number" && typeof b === "number"){
+    if (op === "add"){
+        return a+b
     }
-    return data.join(",")
+    else {
+       return a-b
+    }
+    }
+    
+    return "Invalid operands"
 }
-    return "Invalid data"
-}
-console.log(parseApiResponse("hello"))
-console.log(parseApiResponse(123))
-console.log(parseApiResponse(["a", "b", "c"]))
-console.log(parseApiResponse(["a", 123, "c"]))
-console.log(parseApiResponse([1,2,3]))
-console.log(parseApiResponse({ name: "Tev" }))
+
+
+console.log(calculate(2,3,"add"))
+console.log(calculate(2,"b","subtract"))
+console.log(calculate(false,3,"add"))
+console.log(calculate(2,"3","subtract"))
